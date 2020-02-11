745bac1a08461dd52b6a037d17e754202c97bb62
chadwireman4 <chwi1755@colorado.edu>
2020-02-11 12:08:00 -0700

    fixing errors

--- UtilityFunctions.cpp
+++ UtilityFunctions.cpp
@@ -3,11 +3,11 @@
 #include <iostream>
 std::vector<int> VectorPlusN(std::vector<int> v, int n)
 {
-	for(int i = 0; i < v.size(); i++)
+	for(int i = 0; i < (int) v.size(); i++)
 	{
 		v.at(i)+=n;
 	}
-	for(int j = 0; j < v.size(); j++)
+	for(int j = 0; j < (int) v.size(); j++)
 	{
 		std::cout<<v.at(j)<<std::endl;
 	}
@@ -20,7 +20,7 @@ int Sum(std::vector<int> sums)
 {
 	int sum = 0;
 
-	for(int i = 0; i < sums.size(); i++)
+	for(int i = 0; i < (int) sums.size(); i++)
 	{
 		sum+=sums.at(i);
 	}
