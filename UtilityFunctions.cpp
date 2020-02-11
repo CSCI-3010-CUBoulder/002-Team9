cc1bc23b6b9e6efadb63f8a37143ce4da1309856
Paul <paul92598@gmail.com>
2020-02-11 11:22:49 -0700

    adding files

---
+++ UtilityFunctions.cpp
@@ -0,0 +1,12 @@
+#include "UtilityFunctions.h"
+
+vectorstd::vector<int> VectorPlusN(std::vector<int> v, int n)
+{
+
+}
+
+
+int Sum(std::vector<int> nums)
+{
+
+}
---
+++ UtilityFunctions.h
@@ -0,0 +1,9 @@
+#ifndef UTILITYFUNCTIONS_H
+#define UTILITYFUNCTIONS_H
+
+#include <vector>
+
+
+vectorstd::vector<int> VectorPlusN(std::vector<int> v, int n);
+
+int Sum(std::vector<int> nums);
