df3f4aaa0016ed5550d71cae7f6a78b8dc290048
Rohan Suri <rohansuri17@cu-tcom-4-10.203.26.19.int.colorado.edu>
2020-02-11 11:50:37 -0700

    Added sum function

--- UtilityFunctions.cpp
+++ UtilityFunctions.cpp
@@ -9,14 +9,21 @@ std::vector<int> VectorPlusN(std::vector<int> v, int n)
 	}
 	for(int j = 0; j < v.size(); j++)
 	{
-		std::cout<<v.at(j);
+		std::cout<<v.at(j)<<std::endl;
 	}
 	return v;
 
 }
 
 
-int Sum(std::vector<int> nums)
+int Sum(std::vector<int> sums)
 {
+	int sum = 0;
+
+	for(int i = 0; i < sums.size(); i++)
+	{
+		sum+=sums.at(i);
+	}
+	return sum;
 
 }
--- main.cpp
+++ main.cpp
@@ -5,4 +5,5 @@ int main()
 {
 	std::vector<int> v = {1,2,3,4};
 	VectorPlusN(v,2);
+	std::cout<<Sum(v);
 }
