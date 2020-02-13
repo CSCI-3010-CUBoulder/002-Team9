3224e6c48537d7bf2dc82323d0c1b56358972e3c
Rohan Suri <rohansuri17@cu-tcom-4-10.203.30.111.int.colorado.edu>
2020-02-13 11:23:57 -0700

    Fixed unit test bugs

--- test.cpp
+++ test.cpp
@@ -1,9 +1,12 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "UtilityFunctions.h"
+
 
 TEST_CASE( "Added n to each element of the vector", "[VectorPlusN]" ) {
-    REQUIRE( VectorPlusN({1, 2, 3}, 2) == {3, 4, 5} );
-    REQUIRE( VectorPlusN({4, 5, 6}, 3) == {7, 8, 9} );
+    REQUIRE( VectorPlusN({1, 2, 3}, 2)[0] == 3 );
+    REQUIRE( VectorPlusN({1, 2, 3}, 2)[1] == 4 );
+    REQUIRE( VectorPlusN({1, 2, 3}, 2)[2] == 5 );
 }
 
 TEST_CASE( "Sum vector", "[Sum]" ) {
